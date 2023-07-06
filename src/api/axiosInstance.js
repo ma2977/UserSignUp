2a5cd6ce9b23b998d15231b5c5b071ede91cf044
Maricruz Amurrio <mamurrio89@gmail.com>
2023-07-05 23:15:49 -0400

    4rd try

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://api.render.com/deploy/srv-ciiqcdt9aq012eu1skog?key=PcWkojmgSTg",
+  baseURL: "https://users-crud-5nvg.onrender.com",
 });
